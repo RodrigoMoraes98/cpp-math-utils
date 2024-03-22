c220a2b8a39ef8570010d4a67d3b53bec9052b5a
RodrigoMoraes98 <128483995+RodrigoMoraes98@users.noreply.github.com>
2024-03-22 15:57:24 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 // Copyright 2024 Rodrigo Moraes
 
-#include "include/spacing.h"
 #include <iostream>
+#include "include/spacing.h"
 
 int main(void) {
     std::vector<double> vec;
-    
+
     vec = arange(0.21, 13.4545, 0.866);
 
-    for(int i = 0; i < vec.size(); i++) {
+    for (int i = 0; i < vec.size(); i++) {
         std::cout << vec[i] << std::endl;
     }
 
